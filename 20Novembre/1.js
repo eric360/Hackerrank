29a4dad5c5a0ba158eb34ea1c831974ab040455d
eric360 <eric-richard.hongtuanha@360learning.com>
2015-11-20 16:00:03 +0100

    Optimzed code and Seb's code added for information

--- 20Novembre/1.js
+++ 20Novembre/1.js
@@ -1,6 +1,6 @@
 function processData(input) {
     var array = input.split("\n").map(function (x) {return x.split(" ");})
-    for(var k = 1; k < array.length; k = k + 2){
+    for(var k = 2; k < array.length; k = k + 2){
         var line = array[k]
         if(line.length == 1){
             console.log("YES")
@@ -8,37 +8,32 @@ function processData(input) {
         else if(line.length == 2){
             console.log("NO")
         }else{
-            var sum = sum_(line.map(function(item) { return parseInt(item, 10);}))
-            if(check_(sum)){
+            var lineInt = line.map(function(item) { return parseInt(item, 10);})
+            if(check(sum(lineInt))){
                 console.log("YES")
             }
-            else{console.log("NO")}
+            else{
+                console.log("NO")
+            }
         }
     }
 }
-function check_(array){
+function check(array){
   var lastValue = array[array.length - 1]
   for(var k = 1; k < array.length - 1; k++){
-      var value1 = array[k - 1]
-      var value2 = lastValue - array[k]
+     var value1 = array[k - 1]
+     var value2 = lastValue - array[k]
      if(value1 == value2){
          return true
      }
   }
   return false
 }
-function sum_(array){
+function sum(array){
     var res =[]
     res.push(array[0])
     for(var k = 1; k < array.length; k++){
         res.push(res[k-1]+ array[k])
-    }
-    return res
-}
-function value(array){
-    var res = []
-    for(var k = 0; k < array.length; k = k + 2){
-        res.push(array[k+1])
     }
     return res
 }
---
+++ 20Novembre/1Seb.js
@@ -0,0 +1,39 @@
+function processData(input) {
+    input = input.split("\n");
+    var T = parseInt(input[0], 10), i, k, n, arr;
+    for (i = 0; i < T; ++i) {
+        n = parseInt(input[1 + 2*i], 10);
+        arr = input[2*i + 2].split(" ");
+        for (k = 0; k < n; ++k) {
+            arr[k] = parseInt(arr[k], 10);
+        }
+        solve(arr, n);
+    }
+}
+
+var solve = function (arr, n) {
+    var left = 0,
+        right = sum(arr, n),
+        i;
+    if (left === right) {
+        console.log("YES");
+        return;
+    }
+    for (i = 1; i < n; ++i) {
+        left += arr[i - 1];
+        right -= arr[i];
+        if (left === right) {
+            console.log("YES");
+            return;
+        }
+    }
+    console.log("NO");
+};
+
+var sum = function (arr, n) {
+    var res = 0, i;
+    for (i = 1; i < n; ++i) {
+        res += arr[i];
+    }
+    return res;
+};
