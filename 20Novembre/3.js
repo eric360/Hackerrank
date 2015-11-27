02cd2358ce30336a9e91ba173c5e821b6fdbc0bf
eric360 <eric-richard.hongtuanha@360learning.com>
2015-11-27 15:42:24 +0100

    Key shortened ok

--- 20Novembre/3.js
+++ 20Novembre/3.js
@@ -1,9 +1,13 @@
 function processData(input) {
     var array = input.split("\n").map(function (x) {return x.split(" ");})
     for(var k = 1; k < array.length; k = k + 2){
-        var key = array[k]
+        var key = array[k][0]
         var words = array[k+1]
-        console.log(key)
-        console.log(words)
+        console.log(substitutionDictionary(key))
     }
+}
+function substitutionDictionary(key){
+    var res = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    var keyShortened = key.split("").reverse().join("").replace(/(.)(?=.*\1)/g, "").split("").reverse().join("")
+    return keyShortened
 }
